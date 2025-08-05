ecfa5adf90146ea77d331f3715c4a921c34cc561
lrizzitano <lrizzitano@frba.utn.edu.ar>
2025-08-05 17:06:57 -0300

    correcciones 6ta iteracion

--- src/main/java/org/example/servicioMeteorologico/ServicioMeteorologico.java
+++ src/main/java/org/example/servicioMeteorologico/ServicioMeteorologico.java
@@ -1,24 +1,35 @@
 package org.example.servicioMeteorologico;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public abstract class ServicioMeteorologico {
   protected final HashMap<String, RespuestaDeClima> cacheClimas;
-  protected final HashMap<String, Object> cacheAlertas;
+  protected final HashMap<String, List<String>> cacheAlertas;
   protected final int llamadosDiarios;
+  protected List<InteresadoEnAlertas> interesadoEnAlertas;
 
   public ServicioMeteorologico(int llamadosDiarios) {
-    this.cacheClimas =  new HashMap<>();
+    this.cacheClimas = new HashMap<>();
     this.cacheAlertas = new HashMap<>();
     this.llamadosDiarios = llamadosDiarios;
+    this.interesadoEnAlertas = new ArrayList<>();
   }
 
   abstract protected void actualizarClima(String ciudad);
 
   abstract protected void actualizarAlertas(String ciudad);
 
+  public void agregarInteresado(InteresadoEnAlertas interesadoEnAlertas) {
+    this.interesadoEnAlertas.add(interesadoEnAlertas);
+  }
 
-    public double getTemperatura(String ciudad) {
+  public void quitarInteresado(InteresadoEnAlertas interesadoEnAlertas) {
+    this.interesadoEnAlertas.remove(interesadoEnAlertas);
+  }
+
+  public double getTemperatura(String ciudad) {
     if (!cacheClimas.containsKey(ciudad) || cacheClimas.get(ciudad).expiro()) {
       this.actualizarClima(ciudad);
     }
@@ -32,12 +43,15 @@ public abstract class ServicioMeteorologico {
     return cacheAlertas.get(ciudad);
   }
 
-  public HashMap<String, Object> getAlertasGlobales() {
+  public HashMap<String, List<String>> getAlertasGlobales() {
     return this.cacheAlertas;
   }
 
   public void actualizarAlertasGlobales() {
     this.cacheAlertas.keySet().forEach(this::actualizarAlertas);
+    this.interesadoEnAlertas.forEach((interesadoEnAlertas) -> {
+      interesadoEnAlertas.anteAlerta(cacheAlertas);
+    });
   }
 
 }
